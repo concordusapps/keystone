86377afe386d7b51cae7e0c13c031e0e73a4dce3
Ryan Leckey <rleckey@concordusapps.com>
2013-09-23 19:14:37 -0700

    Add bootstrap.

--- install.sh
+++ install.sh
@@ -3,9 +3,21 @@
 # Keystone 0.1.0 - arch linux automated install procedure
 # -----------------------------------------------------------------------------
 
+# Prerequisites
+# -----------------------------------------------------------------------------
+#  - Somewhat recent arch linux installation media
+#  - Network access
+
 # Instructions
 # -----------------------------------------------------------------------------
 # Boot into the Arch Linux installation media and run:
+#
+#   curl http://git/stash/projects/CAI/repos/keystone/browse/bootstrap.sh\?raw -u <username> | bash
+#
+# Be sure to replace <username> with your LDAP username.
+
+# Configuration
+# -----------------------------------------------------------------------------
 
 # Execute
 # -----------------------------------------------------------------------------
