2fcf5bd79094ea7371d3ff56209df55980860882
Ryan Leckey <rleckey@concordusapps.com>
2013-09-23 19:20:46 -0700

    Add clone procedure.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
-# Bootstrap
-# -----------------------------------------------------------------------------
+# Update the package repositories and ensure we have network access.
 pacman -Syy || exit
+
+# Install the version control system.
+pacman -S git
+
+# Fetch this repository.
+git clone https://git/CAI/keystone.git /tmp/keystone
