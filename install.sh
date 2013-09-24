0665567c1e195f3bb284f0ffeb6e0e573b3071fc
Ryan Leckey <rleckey@concordusapps.com>
2013-09-23 18:47:30 -0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Keystone 0.1.0 - arch linux automated install procedure
+# -----------------------------------------------------------------------------
+
+# Instructions
+# -----------------------------------------------------------------------------
+# Boot into the Arch Linux installation media and run:
+
+# Execute
+# -----------------------------------------------------------------------------
+echo "It's a good day to die."
