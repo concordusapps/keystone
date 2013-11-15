b05064bca400a31dbf3d1c6ecb443bcc78bd48f2
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 08:23:02 -0800

    Discovered this wasn't neccessary.

--- blocks/pacman/powerpill.sh
+++ blocks/pacman/powerpill.sh
@@ -14,26 +14,3 @@ _install_aur 'powerpill'
 # This grabs the latest mirrorlist and then trims it to 15 of the fastest ones.
 # -----------------------------------------------------------------------------
 reflector -f 15 -l 15 > /etc/pacman.d/mirrorlist
-
-# Install CLI JSON parser
-# -----------------------------------------------------------------------------
-_install 'jshon'
-
-# Configure powerpill to use the top 10 rsync servers found by
-# reflector.
-# -----------------------------------------------------------------------------
-local servers
-servers=`reflector -p rsync -f 15 -l 15 | \
-    grep Server | \
-    sed \
-        -e ':a;N;$!ba;s/\n/ -i append/g' \
-        -e 's/Server = / -s /g'`
-
-jshon -e rsync -n array -i servers -p < /etc/powerpill/powerpill.json > /tmp/jshon-$PPID
-mv /tmp/jshon-$PPID /etc/powerpill/powerpill.json
-jshon -e rsync -e servers $servers -i append -p -p < /etc/powerpill/powerpill.json > /tmp/jshon-$PPID
-mv /tmp/jshon-$PPID /etc/powerpill/powerpill.json
-
-# Uninstall CLI JSON parser
-# -----------------------------------------------------------------------------
-_uninstall 'jshon'
