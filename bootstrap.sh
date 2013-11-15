ec1a91c2daf933b481602a88b6b6bdd1008b8d0e
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 04:49:53 -0800

    Add bootstrap script.

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Fetch the tarball from github of the latest version of keystone.
+wget https://github.com/concordusapps/keystone/archive/master.tar.gz
+
+# Extract keystone and navigate inside.
+tar -xzvf master.tar.gz; cd master
+
+# Execute the install script.
+./install.sh
