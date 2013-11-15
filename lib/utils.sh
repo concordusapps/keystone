253d338ffaa3010f33abd59bdf471fbc6852efc8
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 08:48:03 -0800

    Remove debugging.

--- lib/utils.sh
+++ lib/utils.sh
@@ -55,10 +55,6 @@ function _yn {
 # -----------------------------------------------------------------------------
 _print() {
     echo -e "\E[1;37m$1"; tput sgr0
-
-    # DEBUG
-    read -p "Press any key when done... " -n1 -s
-    echo ""
 }
 
 # Countdown
