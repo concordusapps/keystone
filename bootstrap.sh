9e64e18d14305e2e961030b5253816e8070fb694
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 04:53:33 -0800

    Change name.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 wget https://github.com/concordusapps/keystone/archive/master.tar.gz
 
 # Extract keystone and navigate inside.
-tar -xzvf master.tar.gz; cd master
+tar -xzvf master.tar.gz; cd keystone-master
 
 # Execute the install script.
 ./install.sh
