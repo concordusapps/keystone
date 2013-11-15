3cc430a69188fb92c0fb06a38c90d60971abd3ac
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 08:27:09 -0800

    Fix chroot declr.

--- blocks/core/chroot.sh
+++ blocks/core/chroot.sh
@@ -5,7 +5,7 @@ export KEYSTONE_PATH=$(realpath $(dirname $0))
 cp -r $KEYSTONE_PATH $KEYSTONE_MOUNT/root/
 
 # Set that we're in a chroot to the configuration.
-echo "export KEYSTONE_CHROOT=1" >> "/root/$(basename $KEYSTONE_PATH)/config.sh"
+echo "export KEYSTONE_CHROOT=1" >> "$KEYSTONE_MOUNT/root/$(basename $KEYSTONE_PATH)/config.sh"
 
 # Switch to new environment and run the install script.
 arch-chroot $KEYSTONE_MOUNT "/root/$(basename $KEYSTONE_PATH)/install.sh"
