4e8c4895096a77803040642dfb85ec2a90816a6c
Ryan Leckey <rleckey@concordusapps.com>
2013-11-14 21:35:03 -0800

    Remove stuff.

--- blocks/core/chroot.sh
+++ blocks/core/chroot.sh
@@ -8,10 +8,3 @@ cp -r $(realpath $(dirname $0)) $KEYSTONE_MOUNT/root/
 
 # Switch to new environment and run the install script.
 arch-chroot $KEYSTONE_MOUNT "/root/keystone/install.sh"
-
-
-# handle interactively assigned install drive value
-echo -e "#!/bin/bash\nINSTALL_DRIVE=$INSTALL_DRIVE" > "${MNT}${POSTSCRIPT}";
-grep -v "^\s*INSTALL_DRIVE.*" "${0}" >> "${MNT}${POSTSCRIPT}";
-#cp "${0}" "${MNT}${POSTSCRIPT}";
-chmod a+x "${MNT}${POSTSCRIPT}"; arch-chroot "${MNT}" "${POSTSCRIPT}";
