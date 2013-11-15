96f6377f3bbdfd8bf8dfe6ec949c6fbbae7c5542
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 08:24:21 -0800

    We're only in a chroot when we're in a chroot.

--- blocks/core/chroot.sh
+++ blocks/core/chroot.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-# Set that we're in a chroot to the configuration.
-echo "export KEYSTONE_CHROOT=1" >> ./config.sh
-
 # Copy this repository to the new environment.
 export KEYSTONE_PATH=$(realpath $(dirname $0))
 cp -r $KEYSTONE_PATH $KEYSTONE_MOUNT/root/
+
+# Set that we're in a chroot to the configuration.
+echo "export KEYSTONE_CHROOT=1" >> "/root/$(basename $KEYSTONE_PATH)/config.sh"
 
 # Switch to new environment and run the install script.
 arch-chroot $KEYSTONE_MOUNT "/root/$(basename $KEYSTONE_PATH)/install.sh"
