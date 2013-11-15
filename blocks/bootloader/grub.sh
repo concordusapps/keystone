cbba386289f42091a38b1bc6ab8af0b88423d5fb
Ryan Leckey <rleckey@concordusapps.com>
2013-11-15 01:54:45 -0800

    Use magic to figure out device.

--- blocks/bootloader/grub.sh
+++ blocks/bootloader/grub.sh
@@ -4,13 +4,13 @@
 _install 'grub'
 
 # Discern what device /boot is located on.
-if $(mounpoint $KEYSTONE_MOUNT/boot > /dev/null); then
+if $(mountpoint $KEYSTONE_MOUNT/boot > /dev/null); then
     export KEYSTONE_DEVICE_MOUNT=$KEYSTONE_MOUNT/boot
 else
     export KEYSTONE_DEVICE_MOUNT=$KEYSTONE_MOUNT
 fi
 
-export KEYSTONE_DEVICE=$(cat /etc/mtab | awk -v m=/ '{  if ($2 == m) print $1 }')
+export KEYSTONE_DEVICE=$(readlink -e /dev/block/$(mountpoint -d $KEYSTONE_DEVICE_MOUNT | awk -F ':' '{print $1}'):0)
 
 # Install GRUB to the MBR / GPT disk.
-grub-install --root-directory=$KEYSTONE_MOUNT --boot-directory=$KEYSTONE_MOUNT/boot --target=i386-pc --recheck --debug $KEYSTONE_DEVICE
+grub-install --root-directory=$KEYSTONE_MOUNT --boot-directory=$KEYSTONE_MOUNT/boot --target=i386-pc --recheck $KEYSTONE_DEVICE
