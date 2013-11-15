b71e2556b606b4e280540f847ded4eccf6bfee9b
Ryan Leckey <rleckey@concordusapps.com>
2013-11-14 22:54:04 -0800

    Forgot to add binutils.

--- blocks/core/base.sh
+++ blocks/core/base.sh
@@ -8,6 +8,7 @@ rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
 # Install powerpill
 # -----------------------------------------------------------------------------
 # This allows us to fetch all packages concurrently during the pacstrap.
+_install 'binutils'
 _install_aur_manual 'pm2ml'
 _install_aur_manual 'powerpill'
 
